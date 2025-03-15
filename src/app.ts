53da3faacd40c2a799aee8e4b5b77dd56b50049d
Md Kaif Ansari <amdkaif843@gmail.com>
2025-03-15 23:50:16 +0530

    feat: add cookie-parser
    Signed-off-by: Md Kaif Ansari <amdkaif843@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,12 @@ import express from 'express';
 import errorHandler from 'middleware/error';
 import arcjetMiddleware from 'middleware/arcject.middleware';
 import router from './api/v1/routes';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => {
