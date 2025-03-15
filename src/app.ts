9989d720a346dca48dd2f673f24b6bce96ec3968
Md Kaif Ansari <amdkaif843@gmail.com>
2025-03-15 22:14:09 +0530

    feat: add env config
    Signed-off-by: Md Kaif Ansari <amdkaif843@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { config } from 'dotenv';
 config();
+import { PORT } from './config/env';
 
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Server is running');
